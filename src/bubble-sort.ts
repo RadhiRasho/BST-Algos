c983f4bf108e16089651704616017ad2aec7a42f
Radhi Rasho <Radhi.h@outlook.com>
2023-10-09 20:50:23 +0000

    Bubble Sort: Pops into the mix

---
+++ src/bubble-sort.ts
@@ -0,0 +1,25 @@
+function bubbleSort(arr: number[]): number[] {
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = 0; j < arr.length; j++) {
+			if (arr[i] < arr[j]) {
+				let temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+
+	return arr;
+}
+
+console.time('bubbleSort');
+console.timeLog('bubbleSort', bubbleSort([5, 4, 3, 2, 1]));
+console.timeLog(
+	'bubbleSort',
+	bubbleSort([
+		5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2,
+		1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3,
+		2, 1, 5, 4, 3, 2, 1,
+	])
+);
+console.timeEnd('bubbleSort');
