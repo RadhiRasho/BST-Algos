7da30c4e0a4d913aa2f601adcbd87113935fce9d
Radhi Rasho <Radhi.h@outlook.com>
2023-12-05 16:52:38 +0000

    more OR Operator

--- src/bit-manipulation.ts
+++ src/bit-manipulation.ts
@@ -32,11 +32,29 @@ const slug = 0;
 const tug = 101;
 
 if (bug ^ slug) {
-    console.log(bug, slug, bug ^ slug);
+	console.log(bug, slug, bug ^ slug);
 }
 
 if (bug ^ tug) {
-    console.log(bug, tug, bug ^ tug);
+	console.log(bug, tug, bug ^ tug);
 } else {
-    console.log("Else", bug, tug, bug ^ tug);
+	console.log("Else", bug, tug, bug ^ tug);
+}
+
+if (bug | tug) {
+	console.log(bug, tug, bug | tug);
+}
+
+if (bug | slug) {
+	console.log(bug, slug, bug | slug);
+}
+
+if (slug | bug) {
+	console.log(slug, bug, slug | bug);
+}
+
+if (slug | slug) {
+	console.log(slug, slug | slug);
+} else {
+	console.log("Else", slug, slug | slug);
 }
