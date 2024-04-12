3f7af92ec07196e4a5cd72937510ae3ffbd1b83c
Radhi Rasho <Radhi.h@outlook.com>
2024-04-12 13:58:42 +0000

    Implementation of synchronous retry function for the heck of it

--- utils/utils.ts
+++ utils/utils.ts
@@ -37,13 +37,25 @@ export function generateGraph(
 	};
 }
 
-export async function retry<T>(func: () => Promise<T>, threshold = 5) {
+export async function retryAsync<T>(func: () => Promise<T>, threshold = 5) {
 	try {
 		return await func();
 	} catch (err) {
 		if (threshold > 0) {
-			console.log("Retrying...");
-			return await retry<T>(func, threshold - 1);
+			console.log("Asynchronously Retrying...");
+			return await retryAsync<T>(func, threshold - 1);
+		}
+		throw err;
+	}
+}
+
+export async function retry<T>(func: () => T, threshold = 5) {
+	try {
+		return func();
+	} catch (err) {
+		if (threshold > 0) {
+			console.log("Synchronously Retrying...");
+			return retry<T>(func, threshold - 1);
 		}
 		throw err;
 	}
