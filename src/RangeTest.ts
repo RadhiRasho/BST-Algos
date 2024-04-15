d10df6ea225c94ed7497b48b75cb4bcef057f54e
Radhi Rasho <Radhi.h@outlook.com>
2024-04-15 18:07:16 +0000

    Common Types
    Twoslash Query Comments

--- src/RangeTest.ts
+++ src/RangeTest.ts
@@ -1,11 +1,15 @@
 import type { NumberRange, Percent } from "@/types/CommonTypes";
 
 const a: Percent = "0%"; // Min = '0%', Max = '100%'
+//    ^?
 
-//@ts-ignore Error on purpose.
+// @ts-ignore Error on purpose.
 const a2: Percent = "101%"; // Error due to out of range due to starting index of 0;
+//    ^?
 
 const b: NumberRange<101> = 0; // Min = 0, Max = 100;
+//    ^?
 
 //@ts-ignore Error on purpose.
 const b2: NumberRange<101> = 101; // Error Due to out of range due to starting index of 0;
+//    ^?
